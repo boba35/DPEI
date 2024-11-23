f5ec4d1495637100db62651c77bcb3083de4aeb8
basmallasaid <basmala30311@gmail.com>
2024-11-23 23:24:53 +0200

    add new file

---
+++ Csharp/Day1/program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+//using static System.Console;
+
+namespace Day01_G02
+{
+    class program
+    {
+        // case sensitive
+        // entry point
+        public static void Main()
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello Abdoooo");
+
+            Console.ReadKey(); 
+        }
+    }
+}
